e9d5a15c7cb9bd40eda56af1da4046ec8de8820c
Aisha Yusuff <a.yusuff99@outlook.com>
2022-05-12 13:39:16 +0000

    Add first test and method for fizz

---
+++ lib/codewars-fizzbuzz.rb
@@ -0,0 +1,4 @@
+#return an array
+def fizzbuzz(number)
+  return [1,2, 'fizz']
+end
---
+++ spec/codewars-fizzbuzz_spec.rb
@@ -0,0 +1,6 @@
+require 'codewars-fizzbuzz'
+describe 'fizzbuzz' do
+  it 'returns fizz example' do 
+    expect(fizzbuzz(3)).to eq [1, 2, 'fizz']
+  end
+end
